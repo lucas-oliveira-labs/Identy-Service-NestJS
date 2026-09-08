b9e2d122997177c655851725326bdfcf30de0eda
Lucas Oliveira <blesslucas89@gmail.com>
2026-09-08 13:44:49 -0500

    ISN-17 implementando domain e repository

---
+++ src/domain/credential/user-credential.repository.ts
@@ -0,0 +1,18 @@
+import UserCredential from './user-credential.domain';
+
+
+export interface UserCredentialRepository {
+    create(
+        userId: number,
+        password: string,
+    ): Promise<UserCredential>;
+
+    findByUserId(
+        userId: number,
+    ): Promise<UserCredential | null>;
+
+    updatePassword(
+        userId: number,
+        newPassword: string,
+    ): Promise<UserCredential>
+}
