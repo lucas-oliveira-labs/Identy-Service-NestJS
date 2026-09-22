5b07714b22cb31666606317fc28c985cb3db2709
Lucas Oliveira <blesslucas89@gmail.com>
2026-09-22 14:53:25 -0500

    ISN-40-integrando-sessoes-ao-fluxo-de-autenticacao

--- src/application/authentication/refresh-token.ts
+++ src/application/authentication/refresh-token.ts
@@ -1,9 +1,12 @@
 export const REFRESH_TOKEN_SERVICE = Symbol('REFRESH_TOKEN_SERVICE');
 
 export interface RefreshTokenService {
-    generate(userId: number): Promise<string>;
+    generate(userId: number, sessionId: string): Promise<string>;
 
-    validate(token: string): Promise<number | null>;
+    validate(token: string): Promise<{
+        userId: number;
+        sessionId: string;
+    } | null>;
 
     revoke(token: string): Promise<void>;
 }
